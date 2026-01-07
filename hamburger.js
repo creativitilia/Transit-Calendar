1ce30db12e6aa4b148184f05676a44bf21519f78
KALAKHY <majdouline.kalakhy@sogeti.com>
2026-01-07 14:17:35 +0100

    Remove duplicate files from root - keep only scripts/ and styles/ folders

--- hamburger.js
+++
@@ -1,9 +0,0 @@
-export function initHamburger() {
-  const hamburgerButtonElement  = document.querySelector('[data-hamburger-button]');
-
-  hamburgerButtonElement.addEventListener('click', () => {
-    hamburgerButtonElement.dispatchEvent(new CustomEvent('mobile-sidebar-open-request', {
-      bubbles: true,
-      }));
-  });
-}
